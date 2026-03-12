f9eea34dc39ada00ba36b7b4e06e3b4ea39d9deb
Denis Nolting <denis.nolting@gmx.de>
2026-03-12 17:38:49 +0100

    functions navbar quizselction

--- QuizApp/js/script_pick_quiz.js
+++ QuizApp/js/script_pick_quiz.js
@@ -1,8 +1,10 @@
-let allQuizes = ["quiz_1", "quiz_2", "quiz_3", "quiz_4"]
+let allQuizes = ["quiz_navbar_text_1", "quiz_navbar_text_2", "quiz_navbar_text_3", "quiz_navbar_text_4"]
 
 function activateStartButton(id) {
     document.getElementById("button_start_quiz_id").disabled = false;
-    document.getElementById(id).style = "color: white";
-    
+    for (let i = 0; i <  allQuizes.length; i++) {
+        document.getElementById(allQuizes[i]).style = "color: grey";
+    }
+    document.getElementById(id).style = "color: white";   
 }
 
