87fc5ad9e6f6abac434bf45efeb59c814e0140d5
Denis Nolting <denis.nolting@gmx.de>
2026-03-17 15:01:01 +0100

    added function for books stats

---
+++ Bookstore/js/html_tempalte.js
@@ -0,0 +1,49 @@
+function getTemplateBookStatsHTML(i) {
+    return `
+            <div class="book_infobox">
+                <div id="book_infobox_title_id" class="book_infobox_header">
+                    <h2>${books[i].name}</h2>
+                </div>
+
+                <div class="border_line">
+                </div>
+
+                <div class="book_infobox_main">
+                    <div class="book_infobox_main_img">
+                        <img src="./assets/img/book_icon.png">
+                    </div>
+
+                    <div class="border_line">
+                    </div>
+
+                    <div class="book_infobox_main_header">
+                        <h3>${books[i].price.toFixed(2)} €</h3>
+                        <div class="book_infobox_main_header_likes">
+                            <h4>${books[i].likes}</h4>
+                            HERZ
+                        </div>
+                    </div>
+
+                    <table class="book_infobox_main_stats">
+                        <tr>
+                            <td class="table_title">AUTHOR:</td>
+                            <td class="table_value" id="book_infobox_author_id">${books[i].author}</td>
+                        </tr>
+                        <tr>
+                            <td class="table_title">ERSCHEINUNGSJAHR:</td>
+                            <td class="table_value" id="book_infobox_year_id">${books[i].publishedYear}</td>
+                        </tr>
+                        <tr>
+                            <td class="table_title">GENRE:</td>
+                            <td class="table_value" id="book_infobox_genre_id">${books[i].genre}</td>
+                        </tr>
+                    </table>
+
+                    <div class="border_line">
+                    </div>
+
+                </div>
+
+
+            </div>`
+}
--- Bookstore/js/script.js
+++ Bookstore/js/script.js
@@ -4,6 +4,13 @@ function init() {
 }
 
 function renderBooks() {
-    let bookTitleRef = document.getElementById("book_infobox_title_id")
-    bookTitleRef.innerHTML = 
+    let bookInfoboxRef = document.getElementById("section_book_infobox_id");
+    let bookTitleRef = document.getElementById("book_infobox_title_id");
+
+    for (i = 0; i < books.length; i++) {
+        bookInfoboxRef.innerHTML += getTemplateBookStatsHTML(i)
+        for (j = 0; j < books.comments.length; j++){
+            bookInfoboxRef.innerHTML += "ergrb"
+        }
+    }
 }
