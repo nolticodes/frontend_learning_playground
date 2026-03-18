512fec8b69b5f3cbc04028d5bb0d557eac780972
Denis Nolting <denis.nolting@gmx.de>
2026-03-18 13:11:35 +0100

    bug fixes

--- Bookstore/js/script.js
+++ Bookstore/js/script.js
@@ -10,28 +10,38 @@ function renderBooks() {
     for (i = 0; i < books.length; i++) {
         bookInfoboxRef.innerHTML += getTemplateBookStatsHTML(i)
         let bookCommentRef = document.getElementById(`book_infobox_comment_id${i}`);
-        for (j = 0; j < books[i].comments.length; j++){
-            bookCommentRef.innerHTML += getTemplateBookCommentsHTML(i, j)
-        }
-        let likeStatusRef = document.getElementById(`like_img_id_${i}`)
-        if(books[i].liked == true){
-            likeStatusRef.innerHTML += getTemplateLikeButtonLikedHTML(i)
+        if (books[i].comments.length > 0) {
+            for (j = 0; j < books[i].comments.length; j++) {
+                bookCommentRef.innerHTML += getTemplateBookCommentsHTML(i, j)
+            }
         } else {
-            likeStatusRef.innerHTML += getTemplateLikeButtonNotLikedHTML(i)
+            bookCommentRef.innerHTML += "Bisher keine Kommentare"
         }
+        setLikeArea(i);
+    }
+}
+
+function setLikeArea(i) {
+    let likeStatusRef = document.getElementById(`like_img_id_${i}`)
+    if (books[i].liked == true) {
+        likeStatusRef.innerHTML += getTemplateLikeButtonLikedHTML(i)
+    } else {
+        likeStatusRef.innerHTML += getTemplateLikeButtonNotLikedHTML(i)
     }
 }
 
 function likeButton(i) {
     let likeCounterRef = document.getElementById(`like_counter_id_${i}`);
-    if(books[i].liked == true){
+    if (books[i].liked == true) {
         likeCounterRef.innerHTML = Number(likeCounterRef.innerHTML) - 1;
         books[i].liked = false;
+        books[i].likes = books[i].likes - 1
         document.getElementById(`heart_full_id_${i}`).style = "display: none";
         document.getElementById(`heart_empty_id_${i}`).style = "";
     } else {
         likeCounterRef.innerHTML = Number(likeCounterRef.innerHTML) + 1;
         books[i].liked = true;
+        books[i].likes = books[i].likes + 1
         document.getElementById(`heart_full_id_${i}`).style = "";
         document.getElementById(`heart_empty_id_${i}`).style = "display: none";
     }
@@ -41,13 +51,17 @@ function likeButton(i) {
 function addComment(i) {
     let commmentInputRef = document.getElementById(`comment_input_id_${i}`);
 
-    if (commmentInputRef.value != ""){
+    if (commmentInputRef.value != "") {
         books[i].comments.push(commmentInputRef.value);
         commmentInputRef = ""
     }
     saveCommentToLocalStorage(i);
     getCommentsFromLocalStorage(i);
-    renderBooks();
+    renderCommentsCurrentBook(i);
+}
+
+function renderCommentsCurrentBook(i) {
+    let currentCommentRef = document.getElementById(`book_infobox_comment_id${i}`)
 }
 
 // Save Comment to local Storage
@@ -58,7 +72,7 @@ function saveCommentToLocalStorage(i) {
 }
 
 function getCommentsFromLocalStorage(i) {
-    if(localStorageComments === null) {
+    if (localStorageComments === null) {
         books[i].comments
     } else {
         books[i].comments = localStorageComments
