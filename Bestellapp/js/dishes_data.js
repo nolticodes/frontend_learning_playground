3504d92b3953b247f72b5bd8c5dc858b488c989c
Denis Nolting <denis.nolting@gmx.de>
2026-03-20 13:53:31 +0100

    basic html and css structure and content sizing

---
+++ Bestellapp/js/dishes_data.js
@@ -0,0 +1,10 @@
+let dishes = [
+    {
+        "name": "abc",
+        "price": 0.00,
+        "description": "defg",
+        "amount": 0,
+        "imageSource": "./",
+        "ingredients": ["a", "b", "c"]
+    },
+]
