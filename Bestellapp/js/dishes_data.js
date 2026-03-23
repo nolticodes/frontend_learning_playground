3a7e37b996015b85c4ffe3e957e8b30e168baf3f
Denis Nolting <denis.nolting@gmx.de>
2026-03-23 13:31:10 +0100

    added dish array

--- Bestellapp/js/dishes_data.js
+++ Bestellapp/js/dishes_data.js
@@ -1,10 +1,79 @@
 let dishes = [
+    // Krabben Burger
     {
-        "name": "abc",
-        "price": 0.00,
-        "description": "defg",
+        "name": "Klassischer Krabbenburger",
+        "price": 4.99,
+        "description": "Der legendäre Burger nach Geheimrezept – saftig, würzig und einzigartig.",
         "amount": 0,
-        "imageSource": "./",
-        "ingredients": ["a", "b", "c"]
+        "imageSource": "./assets/img/burger_classic.png",
+        "ingredients": ["Brötchen", "Krabbenpatty", "Salat", "Tomate", "Geheimsauce"]
     },
-]
+    {
+        "name": "Doppelter Krabbenburger",
+        "price": 6.99,
+        "description": "Doppelt so viel Geschmack mit zwei saftigen Krabbenpattys.",
+        "amount": 0,
+        "imageSource": "./assets/img/burger_double.png",
+        "ingredients": ["Brötchen", "2x Krabbenpatty", "Käse", "Salat", "Spezialsauce"]
+    },
+    {
+        "name": "Scharfer Krabbenburger",
+        "price": 5.49,
+        "description": "Für alle, die es feurig mögen – mit scharfer Chili-Sauce.",
+        "amount": 0,
+        "imageSource": "./assets/img/burger_spicy.png",
+        "ingredients": ["Brötchen", "Krabbenpatty", "Chili", "Jalapeños", "Scharfe Sauce"]
+    },
+
+    //  Krosse Krabbe Pizza
+    {
+        "name": "Krabbenpizza Classic",
+        "price": 8.99,
+        "description": "Knuspriger Boden mit saftigen Krabben und würziger Tomatensauce.",
+        "amount": 0,
+        "imageSource": "./assets/img/pizza_classic.png",
+        "ingredients": ["Pizzateig", "Tomatensauce", "Krabben", "Käse"]
+    },
+    {
+        "name": "Meeresfrüchte Pizza",
+        "price": 10.49,
+        "description": "Eine maritime Mischung aus Krabben, Muscheln und Garnelen.",
+        "amount": 0,
+        "imageSource": "./assets/img/pizza_seafood.png",
+        "ingredients": ["Pizzateig", "Tomatensauce", "Krabben", "Muscheln", "Garnelen", "Käse"]
+    },
+    {
+        "name": "BBQ Krabbenpizza",
+        "price": 9.99,
+        "description": "Rauchige BBQ-Sauce trifft auf saftige Krabbenstücke.",
+        "amount": 0,
+        "imageSource": "./assets/img/pizza_bbq.png",
+        "ingredients": ["Pizzateig", "BBQ-Sauce", "Krabben", "Zwiebeln", "Käse"]
+    },
+
+    //  Krosse Krabbe Salate
+    {
+        "name": "Krabben Caesar Salat",
+        "price": 7.49,
+        "description": "Frischer Salat mit Krabben, Parmesan und Caesar Dressing.",
+        "amount": 0,
+        "imageSource": "./assets/img/salad_caesar.png",
+        "ingredients": ["Römersalat", "Krabben", "Parmesan", "Croutons", "Caesar Dressing"]
+    },
+    {
+        "name": "Tropischer Krabbensalat",
+        "price": 7.99,
+        "description": "Exotischer Salat mit Ananas und frischen Krabben.",
+        "amount": 0,
+        "imageSource": "./assets/img/salad_tropical.png",
+        "ingredients": ["Salatmix", "Krabben", "Ananas", "Mais", "Joghurt Dressing"]
+    },
+    {
+        "name": "Fitness Krabbensalat",
+        "price": 6.99,
+        "description": "Leichter Salat mit Avocado und proteinreichen Krabben.",
+        "amount": 0,
+        "imageSource": "./assets/img/salad_fitness.png",
+        "ingredients": ["Salatmix", "Krabben", "Avocado", "Gurke", "Limetten Dressing"]
+    }
+];
