affcba4839f7513c775f8fddda43cfc9707328b4
Denis Nolting <denis.nolting@gmx.de>
2026-03-23 18:16:04 +0100

    clean code js

--- Bestellapp/js/html_templates.js
+++ Bestellapp/js/html_templates.js
@@ -9,3 +9,30 @@ function getHTMLForCategories(i) {
             </section>`
 }
 
+function getHTMLForDishes(j) {
+    return `<section class="dish_content_full_width">
+                    <div class="dish_content_max_width">
+                        <div class="dishes_category_list">
+                            <div class="dish_card">
+                                <div class="dish_card_image">
+                                    <img src="./assets/dish_img/dish_0${j}.png">
+                                </div>
+                                <div class="dish_card_stats">
+                                    <div class="dish_card_header">
+                                        <h3>${dishes[j].name}</h3>
+                                        <h3>${dishes[j].price}€</h3>
+                                    </div>
+                                    <div class="dish_card_middle">
+                                        <p>${dishes[j].description}</p>
+                                        <p>Zutaten: ${dishes[j].ingredients}</p>
+                                    </div>
+                                    <div class="dish_card_bottom">
+                                        <button onclick="checkDishInCart(${j})" id="button_not_added_${j}">Add to basket</button>
+                                        <button id="button_added${j}" style="display: none; color: orange">Add to basket</button>                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>`
+}
